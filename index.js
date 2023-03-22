3399a5e28b250d4498f65407777aaa58a5422f3f
Bryant Lin <bryantmlin@gmail.com>
2023-03-22 11:50:15 -0600

    domcontent

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("text").innerHTML = "This is really cool!"
+    
+  });
+  //const test= document.getElementById("text")
+  //test.textContent = "JavaScript is so cool. It lets me add text to my page programmatically."  
+  //test.textContent = "This is really cool!"
+    
